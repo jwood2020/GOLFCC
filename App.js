64d4952831eee4047c22d889e4eaa8dabb37e4e4
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-09-28 21:08:01 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler'; // Import this at the top of your entry file
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './HomeScreen'; // Import the screens
+import LoginPage from './page/Login';
+import SignupPage from './page/Signup';
+
+const Stack = createStackNavigator();
+
+function App() {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="Signup" component={SignupPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
