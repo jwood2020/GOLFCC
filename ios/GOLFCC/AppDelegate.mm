48b693e9ac53afa3d590087b98e87fd008d56506
Taylor Johnson <99372381+Taylorjohnson1@users.noreply.github.com>
2023-10-01 14:43:43 -0500

    changes

--- ios/GOLFCC/AppDelegate.mm
+++ ios/GOLFCC/AppDelegate.mm
@@ -1,19 +1,13 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-<<<<<<< HEAD
-=======
 #import <Firebase.h>
->>>>>>> 64d4952831eee4047c22d889e4eaa8dabb37e4e4
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-<<<<<<< HEAD
-=======
   [FIRApp configure];
->>>>>>> 64d4952831eee4047c22d889e4eaa8dabb37e4e4
   self.moduleName = @"GOLFCC";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
