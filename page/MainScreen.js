0d602d78047c8557da8978da199bdf449e36268c
Taylor Johnson <99372381+Taylorjohnson1@users.noreply.github.com>
2023-10-17 18:29:58 -0500

    Name Change

---
+++ page/MainScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
+
+const Main = () => {
+    return (
+        <Veiw>
+            <Text>Hello World</Text>
+        </Veiw>
+    );
+};
+
+export default Main;
