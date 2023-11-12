c1ec2b9cb5b18d6a5f2f450d8ebec0401c23fe6e
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-11-12 11:30:10 -0600

    Fix an error 'cannot update component' caused by trying to run the rest of HomeScreen when CheckLoggedIn returns true

--- page/HomeScreen.js
+++ page/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import background from '../backgroundimage.jpeg';
@@ -8,9 +8,14 @@ import CheckLoggedIn from '../firebase/CheckLoggedIn';
 function HomeScreen() {
   const navigation = useNavigation();
 
-  if (CheckLoggedIn()) {
-    navigation.navigate('MainContainer');
-  }
+  /* forces this to only run once, synchronously */
+  useEffect(() => {
+
+    /* If user is already logged in then the user does not need to log in again */
+    if (CheckLoggedIn()) {
+      navigation.navigate('MainContainer');
+    }
+  }, []);
 
   const handleLogin = () => {
     // Navigate to the AnotherScreen
@@ -24,23 +29,22 @@ function HomeScreen() {
 
   return (
     <View style={styles.root}>
-
-            <ImageBackground
-              style={styles.container}
-              imageStyle={styles.image}
-              source={background}
-            >
-              <Text style={styles.heading}>GOLFCC</Text>
-              <Text style={styles.text}>“Success in this game depends less on strength of body than strength of mind and character.”</Text>
-              <Text style={styles.quotetext}>-Arnold Palmer</Text>
-              <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                <Text style={styles.buttonText}>Login</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={handleSignup}>
-                <Text style={styles.buttonText}>Signup</Text>
-              </TouchableOpacity>
-            </ImageBackground>
-        </View>
+          <ImageBackground
+            style={styles.container}
+            imageStyle={styles.image}
+            source={background}
+          >
+            <Text style={styles.heading}>GOLFCC</Text>
+            <Text style={styles.text}>“Success in this game depends less on strength of body than strength of mind and character.”</Text>
+            <Text style={styles.quotetext}>-Arnold Palmer</Text>
+            <TouchableOpacity style={styles.button} onPress={handleLogin}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleSignup}>
+              <Text style={styles.buttonText}>Signup</Text>
+            </TouchableOpacity>
+          </ImageBackground>
+    </View>
       
   );
 }
