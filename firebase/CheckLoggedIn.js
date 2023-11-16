25688dea1077282da40151330204b332e93c68cc
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-11-16 11:25:53 -0600

    Log the user in the console

--- firebase/CheckLoggedIn.js
+++ firebase/CheckLoggedIn.js
@@ -2,6 +2,7 @@ import firebase from '@react-native-firebase/app';
 import auth  from '@react-native-firebase/auth';
 
 function CheckLoggedIn() {
+    console.log(firebase.app().auth().currentUser);
     return firebase.app().auth().currentUser;
 }
 
