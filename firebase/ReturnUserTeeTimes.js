8da799a2ec5a262e86f178dfce84501aed9760ba
jwood2020 <99372318+jwood2020@users.noreply.github.com>
2023-11-27 17:01:30 -0600

    new file to return the tee times a user created

---
+++ firebase/ReturnUserTeeTimes.js
@@ -0,0 +1,65 @@
+/* firebase/ReturnUserTeeTimes.js
+   Taylor Johnson and Jacob Woodmancy
+   CS 391 - Senior Project
+   GOLFCC
+   12-14-2023
+
+   Description: This program reads the authentication information from the 
+   user, reads the recent tee times records, and then returns which of those
+   records were created by that user.
+*/
+
+import firebase from '@react-native-firebase/app';
+import auth  from '@react-native-firebase/auth';
+import { ref, onValue, getDatabase } from '@react-native-firebase/database';
+import { useState, useEffect } from 'react';
+
+
+function ReturnUserTeeTimes() {
+
+    /* Read the unique identifier from the current user's authentication */
+    const auth_uid = firebase.app().auth().currentUser.uid;
+
+    /* db_data object used to store the database read */
+    const [db_data, setdb_data] = useState({});
+
+    const db = getDatabase();
+    const data_ref = ref(db, "/TeeTimes");
+
+    /* useEffect will prevent the onValue function from rerendering the function, */
+    /* meaning it will only run once. Inside the useEffect we will read the */
+    /* database information from the TeeTimes key. */
+    useEffect(() => {
+        return onValue(data_ref, querySnapShot => {
+            let data = querySnapShot.val() || {};
+            let dbItems = {...data};
+            setdb_data(dbItems);
+        });
+    }, []);
+
+    user_teeTimes = {}
+
+    for (id in db_data) {
+        for (day in db_data[id]) {
+            for (time in db_data[id][day]) {
+                for (player in db_data[id][day][time]) {
+                    if (db_data[id][day][time][player]['created_by'] === auth_uid) {
+                        console.log("inside if");
+                        if (user_teeTimes[day] === undefined) {
+                            user_teeTimes[day] = {};
+                        }
+                        if (user_teeTimes[day][time] === undefined) {
+                            user_teeTimes[day][time] = {};
+                        }
+                        user_teeTimes[day][time][player] = db_data[id][day][time][player];
+                    }
+                }
+            }
+        }
+    }
+
+    return user_teeTimes;
+
+}
+
+export default ReturnUserTeeTimes;
